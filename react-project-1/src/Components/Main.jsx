a893ca7418a915f4f3b66cfe7577748ef00d828e
jul1998 <jguevara321@gmail.com>
2022-09-27 17:53:18 -0600

    first commit

---
+++ react-project-1/src/Components/Main.jsx
@@ -0,0 +1,19 @@
+
+export default function Main() {
+    return (
+        <div className="maincontent">
+            <h2 className="subheader">
+                Fun facts about React
+            </h2>
+
+            <ul className="list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+
+            </ul>
+        </div>
+        )
+}
